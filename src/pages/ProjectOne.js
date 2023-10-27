8e46a16ae896a461add45671d983fb2dfb248b08
Michael <88445485+mjnyland@users.noreply.github.com>
2023-10-27 16:21:29 -0400

    Added favicon

--- src/pages/ProjectOne.js
+++ src/pages/ProjectOne.js
@@ -17,36 +17,38 @@ function ProjectOne() {
           </div>
         </Link>
 
-        <div className="flex flex-col lg:justify-between lg:flex-row">
-          <p>Personalization at Spotify</p>
-          <p>
-            The talented machine learning engineers at Spotify needed a look and
-            feel to match the exciting work they were doing at the intersection
-            of music and AI. I worked directly with VPs in engineering, product,
-            and design to establish typography, color, and layout styling that
-            aligned with their mission statement as an organization.
-          </p>
-        </div>
+        <img src={imageOne} alt="AI at Spotify" />
 
-        <div className="flex flex-row">
-          <div className="flex flex-col">
-            <p>Details:</p>
-            <div>
-              <p>2022</p>
-              <p>Spotify</p>
-              <p>Internal Brand Designer</p>
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <p>Details:</p>
-            <p>
-              Visual details were adjusted after my layoff. They don’t know what
-              they’re missing ;)
+        <div className="flex flex-col gap-[16px]">
+          <div className="flex flex-col lg:justify-between lg:flex-row">
+            <p>Personalization at Spotify</p>
+            <p className="max-w-lg">
+              The talented machine learning engineers at Spotify needed a look
+              and feel to match the exciting work they were doing at the
+              intersection of music and AI. I worked directly with VPs in
+              engineering, product, and design to establish typography, color,
+              and layout styling that aligned with their mission statement as an
+              organization.
             </p>
           </div>
+          <div className="flex flex-row ml-auto max-w-lg">
+            <div className="flex flex-col">
+              <p>Details:</p>
+              <div>
+                <p>2022</p>
+                <p>Spotify</p>
+                <p>Internal Brand Designer</p>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <p>Note:</p>
+              <p>
+                Visual details were adjusted after my layoff. They don’t know
+                what they’re missing ;)
+              </p>
+            </div>
+          </div>
         </div>
-
-        <img src={imageOne} alt="AI at Spotify" />
         <img src={imageTwo} alt="AI at Spotify" />
         <img src={imageThree} alt="AI at Spotify" />
         <img src={imageFour} alt="AI at Spotify" />
