be597c70a9858fdf4c4d3e969f202bed8fe832c5
Michael <88445485+mjnyland@users.noreply.github.com>
2023-11-10 15:19:40 -0500

    spacing on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,22 +3,23 @@ import { Outlet, Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className="flex flex-col items-left m-[160px] gap-[64px]">
-      <h1 className="text-8xl font-semibold">Michael Nyland</h1>
+    <div className="flex flex-col items-left gap-[64px] mx-auto max-w-4xl">
+      <h1 className="text-8xl font-semibold pt-[160px]">Michael Nyland</h1>
       <div className="flex flex-col lg:flex-row justify-between flex-grow">
-        <div className="flex flex-col gap-[16px]">
+        <div className="flex flex-col gap-[12px]">
+          <h3 className="text-2xl pb-[8px]">Experience:</h3>
           <div>
-            <h1 className="text-2xl font-bold">Daysheets</h1>
+            <h1 className="text-2xl font-semibold pb-[4px]">Daysheets</h1>
             <h1 className="text-xl">Product & Brand Designer</h1>
             <h1 className="text-xl">2023 - Present</h1>
           </div>
           <div>
-            <h1 className="text-2xl font-bold">Spotify</h1>
+            <h1 className="text-2xl font-semibold pb-[4px]">Spotify</h1>
             <h1 className="text-xl">Internal Brand Designer</h1>
             <h1 className="text-xl">2022 - 2023</h1>
           </div>
           <div>
-            <h1 className="text-2xl font-bold">Banner</h1>
+            <h1 className="text-2xl font-semibold pb-[4px]">Banner</h1>
             <h1 className="text-xl">Motion Designer</h1>
             <h1 className="text-xl">2020 - 2022</h1>
           </div>
