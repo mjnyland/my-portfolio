03d86628729d1b6934f667845a4cdd7af10e96bf
Michael <88445485+mjnyland@users.noreply.github.com>
2024-02-16 16:03:09 -0500

    Styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,20 +6,19 @@ function Home() {
   return (
     <div className="flex flex-col gap-[120px] mx-[80px] my-[160px] pb-[160px]">
       <div className="flex flex-col max-w-3xl pb-[80px]">
-        <h1 className="text-4xl font-semibold">Hi there, I'm Michael.</h1>
+        <p className="text-3xl">{"Hi! I'm Michael."}</p>
         <div className="flex flex-col gap-[16px]">
           <p className="text-2xl lg:text-3xl pt-[32px]">
-            I'm a designer with 6+ years of experience. I'm currently in charge
-            of Product & Brand Design at{" "}
+            I'm a product and brand designer with 6 years of experience.
+            Currently I'm the lead designer at{" "}
             <a
-              className="font-bold cursor-pointer"
+              className="font-regular cursor-pointer underline underline-offset-2"
               href="https://www.daysheets.com"
               target="_blank"
             >
               Daysheets.
             </a>{" "}
-            I live in Brooklyn, by way of Chicago. I'm currently looking for
-            full-time roles with early-stage startups.
+            I'm looking for full-time roles with early-stage startups.
           </p>
           {/*<a
             className="font-semibold cursor-pointer text-blue-600 tracking-tight underline"
@@ -62,45 +61,41 @@ function Home() {
 
       <div className="flex flex-col">
         {/* Projects Container */}
-        <div className="flex flex-col items-left gap-[16px]">
-          <div className="pb-[32px] gap-[16px] flex flex-col">
-            <h3 className="text-3xl lg:text-3xl pb-[16px]">
-              Selected Projects
-            </h3>
+        <div className="pb-[32px] flex flex-col gap-[8px]">
+          <h3 className="text-3xl lg:text-3xl">Selected Projects</h3>
+          <hr />
+          <div className="flex flex-col items-left justify-center gap-[32px]">
+            <Link className="font-blue pt-[24px]" to="ProjectFour">
+              <h1 className="text-6xl pb-[8px] tracking-[-1px]">Daysheets</h1>
+              <p className="text-xl">
+                Product Design, User Research, Prototyping
+              </p>
+            </Link>
             <hr />
-            <div className="flex flex-col items-left justify-center gap-[40px]">
-              <Link className="font-blue" to="ProjectFour">
-                <h1 className="text-6xl pb-[8px] tracking-tight">Daysheets</h1>
-                <p className="text-xl">
-                  Product Design, User Research, Prototyping
-                </p>
-              </Link>
-              <hr />
-              <Link className="font-blue" to="ProjectOne">
-                <h1 className="text-6xl pb-[8px] tracking-tight">
-                  AI at Spotify
-                </h1>
-                <p className="text-xl">
-                  Brand Idenity, Visual Design, Presentation Design
-                </p>
-              </Link>
-              <hr />
-              <Link className="font-blue" to="ProjectTwo">
-                <h1 className="text-6xl pb-[8px] tracking-tight">
-                  SBWC Mobile App
-                </h1>
-                <p className="text-xl">
-                  Product Design, User Research, Prototyping
-                </p>
-              </Link>
-              <hr />
-              <Link className="font-blue" to="ProjectThree">
-                <h1 className="text-6xl pb-[8px] tracking-tight">Crewsheets</h1>
-                <p className="text-xl">
-                  Product Design, Visual Design, Prototyping
-                </p>
-              </Link>
-            </div>
+            <Link className="font-blue" to="ProjectOne">
+              <h1 className="text-6xl pb-[8px] tracking-[-1px]">
+                AI at Spotify
+              </h1>
+              <p className="text-xl">
+                Brand Idenity, Visual Design, Presentation Design
+              </p>
+            </Link>
+            <hr />
+            <Link className="font-blue" to="ProjectTwo">
+              <h1 className="text-6xl pb-[8px] tracking-[-1px]">
+                SBWC Mobile App
+              </h1>
+              <p className="text-xl">
+                Product Design, User Research, Prototyping
+              </p>
+            </Link>
+            <hr />
+            <Link className="font-blue" to="ProjectThree">
+              <h1 className="text-6xl pb-[8px] tracking-[-1px]">Crewsheets</h1>
+              <p className="text-xl">
+                Product Design, Visual Design, Prototyping
+              </p>
+            </Link>
           </div>
         </div>
       </div>
