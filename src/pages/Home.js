68643f694ba227d5d616e65e95a7db84a9212491
Michael <88445485+mjnyland@users.noreply.github.com>
2024-04-11 12:18:20 -0400

    home styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import "../App.css";
 import { Outlet, Link } from "react-router-dom";
 import ReactPlayer from "react-player";
-import { HiExternalLink } from "react-icons/hi";
+import { HiExternalLink, HiArrowNarrowRight } from "react-icons/hi";
 
 function Home() {
   return (
     <div className="flex flex-col gap-[64px] mt-[160px] max-w-xl px-[32px] mx-auto">
-      <div className="flex flex-col">
+      <div className="flex flex-col p-[4px]">
         <p className="text-3xl">{"Michael Nyland"}</p>
         <div className="flex flex-col">
           <p className="text-base pt-[32px] pb-[24px]">
@@ -47,42 +47,75 @@ function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col max-w-">
         {/* Projects Container */}
         <div className="flex flex-col">
-          <h3 className="text-base text-gray-500">Projects</h3>
+          <h3 className="text-base text-gray-500 p-[4px] mb-[8px]">Projects</h3>
           <div className="flex flex-col items-left justify-center gap-[16px]">
-            <Link className="font-blue pt-[24px]" to="Daysheets">
-              <h1 className="text-base font-medium hover:underline">
-                Daysheets
-              </h1>
-              <p className="text-sm text-gray-500">
-                Product Design, User Research, Prototyping
-              </p>
+            <Link
+              className="font-blue p-[4px] flex flex-row hover:bg-gray-100 rounded group"
+              to="Daysheets"
+            >
+              <div className="flex flex-col">
+                <h1 className="text-base font-medium group-hover:underline">
+                  Daysheets
+                </h1>
+                <p className="text-sm text-gray-500">
+                  Product Design, User Research, Prototyping
+                </p>
+              </div>
+              <div className="my-auto ml-auto pr-[16px] hidden group-hover:block">
+                <HiArrowNarrowRight />
+              </div>
             </Link>
-            <Link className="font-blue" to="Spotify">
-              <h1 className="text-base font-medium hover:underline">
-                AI at Spotify
-              </h1>
-              <p className="text-sm text-gray-500">
-                Brand Idenity, Motion Design, Presentation Design
-              </p>
+
+            <Link
+              className="font-blue p-[4px] flex flex-row hover:bg-gray-100 rounded group"
+              to="Spotify"
+            >
+              <div>
+                <h1 className="text-base font-medium group-hover:underline">
+                  AI at Spotify
+                </h1>
+                <p className="text-sm text-gray-500">
+                  Brand Idenity, Motion Design, Presentation Design
+                </p>
+              </div>
+              <div className="my-auto ml-auto pr-[16px] hidden group-hover:block">
+                <HiArrowNarrowRight />
+              </div>
             </Link>
-            <Link className="font-blue" to="SBWC">
-              <h1 className="text-base font-medium hover:underline ">
-                SBWC Mobile App
-              </h1>
-              <p className="text-sm text-gray-500">
-                Product Design, Prototyping
-              </p>
+            <Link
+              className="font-blue p-[4px] flex flex-row hover:bg-gray-100 rounded group"
+              to="SBWC"
+            >
+              <div>
+                <h1 className="text-base font-medium group-hover:underline ">
+                  SBWC Mobile App
+                </h1>
+                <p className="text-sm text-gray-500">
+                  Product Design, Prototyping
+                </p>
+              </div>
+              <div className="my-auto ml-auto pr-[16px] hidden group-hover:block">
+                <HiArrowNarrowRight />
+              </div>
             </Link>
-            <Link className="font-blue" to="Crewsheets">
-              <h1 className="text-base font-medium hover:underline">
-                Crewsheets
-              </h1>
-              <p className="text-sm text-gray-500">
-                Product Design, Visual Design, Prototyping
-              </p>
+            <Link
+              className="font-blue p-[4px] flex flex-row hover:bg-gray-100 rounded group"
+              to="Crewsheets"
+            >
+              <div>
+                <h1 className="text-base font-medium group-hover:underline">
+                  Crewsheets
+                </h1>
+                <p className="text-sm text-gray-500">
+                  Product Design, Visual Design, Prototyping
+                </p>
+              </div>
+              <div className="my-auto ml-auto pr-[16px] hidden group-hover:block">
+                <HiArrowNarrowRight />
+              </div>
             </Link>
           </div>
         </div>
