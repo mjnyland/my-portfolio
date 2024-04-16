331eef497b7528166ff056d8aa3850899b98df3a
Michael <88445485+mjnyland@users.noreply.github.com>
2024-04-16 10:49:01 -0400

    text sizes

--- src/pages/AMA.jsx
+++ src/pages/AMA.jsx
@@ -21,7 +21,7 @@ const AMA = () => {
                 <p className="font-medium">
                   What is the project you're most proud of?
                 </p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   Daysheets mobile app redesign. Check it out{" "}
                   <Link to={"/Daysheets"} className="underline cursor-pointer">
                     here
@@ -39,7 +39,7 @@ const AMA = () => {
                   Do you consider yourself a brand designer or a product
                   designer?
                 </p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   I've done both. Currently I'm focused on product. That said, I
                   love integrating brand with product and vice versa. That's why
                   I love working with startups. There's usually a need for that
@@ -53,7 +53,7 @@ const AMA = () => {
                 <p className="font-medium">
                   What about coding - are you a designer or an engineer?
                 </p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   Again, I've done both. I get that my background reads as "Jack
                   of all trades, master of none", but I let my curiosity be my
                   compass. I have a technical side to me that if I ignore, I
@@ -62,9 +62,7 @@ const AMA = () => {
                   Currently focusing on Swift and SwiftUI.
                 </p>
               </div>
-              <p className="text-sm text-gray-500 ml-auto hidden">
-                Dec. 2, 2024
-              </p>
+              <p className=" text-gray-500 ml-auto hidden">Dec. 2, 2024</p>
             </div>
 
             <div className="flex flex-row">
@@ -72,7 +70,7 @@ const AMA = () => {
                 <p className="font-medium">
                   What is the hardest thing about product vs brand?
                 </p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   Brand designs are seen and interacted with, but they're not
                   really used. Products are used and relied on every day. That
                   requires a deep level of understanding of the customer and
@@ -86,52 +84,46 @@ const AMA = () => {
             <div className="flex flex-row">
               <div className="flex flex-col gap-[8px]">
                 <p className="font-medium">What makes you so confident?</p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   I've made just about every mistake you can make. Every time
                   I've learned from it and gotten better. (Cliche, I know, but
                   true)
                 </p>
               </div>
-              <p className="text-sm text-gray-500 ml-auto hidden">
-                Dec. 2, 2024
-              </p>
+              <p className=" text-gray-500 ml-auto hidden">Dec. 2, 2024</p>
             </div>
 
             <div className="flex flex-row">
               <div className="flex flex-col gap-[8px]">
                 <p className="font-medium">What are your weaknesses?</p>
-                <p className="text-sm text-gray-500">
+                <p className=" text-gray-500">
                   Sometimes I overthink. Sometimes thats a good thing, but most
                   times it's better to move fast and get feedback. I'm working
                   on this.
                 </p>
               </div>
-              <p className="text-sm text-gray-500 ml-auto hidden">
-                Dec. 2, 2024
-              </p>
+              <p className=" text-gray-500 ml-auto hidden">Dec. 2, 2024</p>
             </div>
 
             <div className="flex flex-row">
               <div className="flex flex-col gap-[8px]">
                 <p className="font-medium">What are your strengths?</p>
                 <div className="flex flex-col gap-[8px]">
-                  <p className="text-sm text-gray-500">Two things:</p>
-                  <p className="text-sm text-gray-500">
+                  <p className=" text-gray-500">Two things:</p>
+                  <p className=" text-gray-500">
                     Design isn't linear - it's messy. I don't bother with
                     step-by-step processes because it's unrealistic. I thrive in
                     vague, ever-changing environments. I consider this my
                     greatest strength.
                   </p>
-                  <p className="text-sm text-gray-500">
+                  <p className=" text-gray-500">
                     I understand that trust goes both ways. I weight subject
                     experts' opinion heavily when making decisions, but I can
                     also communicate my own thoughts without being a jerk.
                   </p>
                 </div>
               </div>
-              <p className="text-sm text-gray-500 ml-auto hidden">
-                Dec. 2, 2024
-              </p>
+              <p className=" text-gray-500 ml-auto hidden">Dec. 2, 2024</p>
             </div>
 
             <div className="pt-[32px]">
@@ -140,7 +132,7 @@ const AMA = () => {
                 alt="A picture from my honeymoon"
                 className="rounded-lg"
               />
-              <p className="text-sm text-gray-500 pt-[8px]">
+              <p className=" text-gray-500 pt-[8px]">
                 Looking over Stockholm | 2021
               </p>
             </div>
@@ -148,8 +140,8 @@ const AMA = () => {
             <div className="flex flex-col gap-[16px] pt-[32px]">
               <hr />
               <div className="flex">
-                <p className="text-gray-500 text-sm">Updated Apr. 2024</p>
-                <p className="text-gray-500 text-sm ml-auto">Made in NYC 🗽</p>
+                <p className="text-gray-500 ">Updated Apr. 2024</p>
+                <p className="text-gray-500  ml-auto">Made in NYC 🗽</p>
               </div>
             </div>
           </div>
